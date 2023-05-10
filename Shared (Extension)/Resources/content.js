743fa65d1e698dbcd4ac704b1e18d7f7c3d38143
Jack Allen <jackallensemail+git@gmail.com>
2023-05-10 20:30:33 +0100

    init

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,39 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+var loaded = false
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+function load() {
+    if (loaded) {
+        return
+    }
+    
+    var url = "https://cdn.jsdelivr.net/gh/share-session/browser@1.2.0/dist/content.js?e=" + Date.now()
+
+    var script = document.createElement("script")
+    
+    var id = "share-session"
+
+    script.id = id
+    script.src = url
+    script.async = true;
+    script.language = "javascript"
+    
+    if (document.getElementById(id)) {
+        return;
+    }
+
+    document.body.appendChild(script);
+}
+
+function tryLoad() {
+    if (loaded) {
+        return;
+    }
+    
+    if (document.body == null) {
+        setTimeout(tryLoad, 1000)
+        return
+    }
+    
+    load();
+}
+
+tryLoad()
