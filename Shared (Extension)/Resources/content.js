9ae91576c4b137f161711d2ea90ec258d7332e36
Jack Allen <jackallensemail+git@gmail.com>
2023-05-15 22:17:02 +0100

    share session

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,7 +5,7 @@ function load() {
         return
     }
     
-    var url = "https://cdn.jsdelivr.net/gh/share-session/browser@1.8.0/dist/content.js?e=" + Date.now()
+    var url = "https://cdn.jsdelivr.net/gh/share-session/browser@1.11.0/dist/content.js?e=" + Date.now()
 
     var script = document.createElement("script")
     
